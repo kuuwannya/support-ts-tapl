92aa84d4f068aaab9c74de41d1350e8a617ff010
kuuwannya <kuuuuu05062525@gmail.com>
2026-01-28 20:47:49 +0900

    expandType関数の追加

--- base.ts
+++ base.ts
@@ -92,6 +92,27 @@ function subtype(ty1: Type, ty2: Type): boolean {
       return true;
   }
 }
+//
+function expandType(ty: Type, tyVarName: string, repTy: Type): Type {
+  switch (ty.tag) {
+    case "Boolean":
+    case "Number":
+      return ty;
+    case "Func":
+      const params = ty.params.map((name, type) => ({ name, type: expandType(type, tyVarName, repTy) }));
+      const retType = expandType(ty.retType, tyVarName, repTy);
+      return { tag: "Func", params, retType };
+    case "Object":
+      const props = ty.props.map(({ name, type }) => ({ name, type: expandType(type, tyVarName, repTy) }));
+      return { tag: "Object", props };
+    case "Rec":
+      if (ty.name === tyVarName) return ty;
+      const newType = expandType(ty.type, tyVarName, repTy);
+      return { tag: "Rec", name: ty.name, type: newType };
+    case "TypeVar":
+      return (ty.name === tyVarName) ? repTy : ty;
+  }
+}
 
 
 function typecheck(t: Term, tyEnv: TypeEnv): Type {
